9e79ddc6c79a76e27304b2da79829ab346a43be1
Hazewu <15320519577@163.com>
2019-07-09 10:18:21 +0800

    s1_2

---
+++ s1_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    char c;
+    float f;
+    cin>>i>>c>>f;                         //consecutive input
+    cout<<"i="<<i<<" c="<<c<<" f="<<f;    //consecutive output
+    return 0;    
+}
+/*
+    the data type entered must be line with the data type to be extracted
+	space and carriage return can't be input by cin
+	also the character string can't include space
+*/
